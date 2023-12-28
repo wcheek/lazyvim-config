91bc867b25899bec8425320927083408d9a33aae
Wesley Cheek <66931618+wcheek@users.noreply.github.com>
2023-12-28 10:45:53 +0900

    Change theme again

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,142 @@
 return {
   -- { "shaunsingh/nord.nvim" },
-  { "Mofiqul/dracula.nvim" },
+  -- { "Mofiqul/dracula.nvim" },
   -- {
   --   "catppuccin/nvim",
+  --   lazy = true,
   --   name = "catppuccin",
-  --   priority = 1000,
-  --   ---@class CatppuccinOptions
   --   opts = {
-  --     transparent_background = true,
+  --     flavour = "macchiato",
+  --     background = { -- :h background
+  --       light = "latte",
+  --       dark = "mocha",
+  --     },
+  --     transparent_background = false, -- disables setting the background color.
+  --     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+  --     term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+  --     dim_inactive = {
+  --       enabled = false, -- dims the background color of inactive window
+  --       shade = "dark",
+  --       percentage = 0.15, -- percentage of the shade to apply to the inactive window
+  --     },
+  --     no_italic = false, -- Force no italic
+  --     no_bold = false, -- Force no bold
+  --     no_underline = false, -- Force no underline
+  --     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+  --       comments = { "italic" }, -- Change the style of comments
+  --       conditionals = { "italic" },
+  --       loops = {},
+  --       functions = {},
+  --       keywords = {},
+  --       strings = {},
+  --       variables = {},
+  --       numbers = {},
+  --       booleans = {},
+  --       properties = {},
+  --       types = {},
+  --       operators = {},
+  --     },
+  --     color_overrides = {},
+  --     custom_highlights = {},
+  --     integrations = {
+  --       aerial = true,
+  --       alpha = true,
+  --       cmp = true,
+  --       dashboard = true,
+  --       flash = true,
+  --       gitsigns = true,
+  --       headlines = true,
+  --       illuminate = true,
+  --       indent_blankline = { enabled = true },
+  --       leap = true,
+  --       lsp_trouble = true,
+  --       mason = true,
+  --       markdown = true,
+  --       mini = true,
+  --       native_lsp = {
+  --         enabled = true,
+  --         underlines = {
+  --           errors = { "undercurl" },
+  --           hints = { "undercurl" },
+  --           warnings = { "undercurl" },
+  --           information = { "undercurl" },
+  --         },
+  --       },
+  --       navic = { enabled = true, custom_bg = "lualine" },
+  --       neotest = true,
+  --       neotree = true,
+  --       noice = true,
+  --       notify = true,
+  --       semantic_tokens = true,
+  --       telescope = true,
+  --       treesitter = true,
+  --       treesitter_context = true,
+  --       which_key = true,
+  --     },
   --   },
   -- },
   {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      --- @usage 'auto'|'main'|'moon'|'dawn'
+      variant = "moon",
+      --- @usage 'main'|'moon'|'dawn'
+      dark_variant = "main",
+      bold_vert_split = false,
+      dim_nc_background = false,
+      disable_background = false,
+      disable_float_background = false,
+      disable_italics = false,
+
+      --- @usage string hex value or named color from rosepinetheme.com/palette
+      groups = {
+        background = "base",
+        background_nc = "_experimental_nc",
+        panel = "surface",
+        panel_nc = "base",
+        border = "highlight_med",
+        comment = "muted",
+        link = "iris",
+        punctuation = "subtle",
+
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        warn = "gold",
+
+        headings = {
+          h1 = "iris",
+          h2 = "foam",
+          h3 = "rose",
+          h4 = "gold",
+          h5 = "pine",
+          h6 = "foam",
+        },
+        -- or set all headings at once
+        -- headings = 'subtle'
+      },
+
+      -- Change specific vim highlight groups
+      -- https://github.com/rose-pine/neovim/wiki/Recipes
+      highlight_groups = {
+        ColorColumn = { bg = "rose" },
+
+        -- Blend colours against the "base" background
+        CursorLine = { bg = "foam", blend = 10 },
+        StatusLine = { fg = "love", bg = "love", blend = 10 },
+
+        -- By default each group adds to the existing config.
+        -- If you only want to set what is written in this config exactly,
+        -- you can set the inherit option:
+        Search = { bg = "gold", inherit = false },
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "dracula",
+      colorscheme = "rose-pine",
     },
   },
 }
--- lua/plugins/dartls.lua
+++ lua/plugins/dartls.lua
@@ -1,12 +1,12 @@
 -- Copy this file to your ~/.config/nvim/lua/plugins/
 -- This file was written by @cddm. See https://www.reddit.com/r/neovim/comments/14c5e6o/how_to_set_up_dartflutter_with_neovim/jojf4z5/
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        dartls = {},
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       dartls = {},
+  --     },
+  --   },
+  -- },
 }
