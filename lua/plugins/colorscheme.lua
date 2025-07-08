42bb6fe58ef5d420ebc837adfa452f9f9abc38c8
Wesley Jon Cheek <w-cheek@satake-japan.co.jp>
2025-07-08 09:26:21 +0900

    lazyvim config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
   -- { "shaunsingh/nord.nvim" },
-  -- { "Mofiqul/dracula.nvim" },
+  { "Mofiqul/dracula.nvim" },
   {
     "catppuccin/nvim",
     lazy = true,
@@ -75,64 +75,64 @@ return {
       },
     },
   },
-  -- {
-  --   "rose-pine/neovim",
-  --   name = "rose-pine",
-  --   opts = {
-  --     --- @usage 'auto'|'main'|'moon'|'dawn'
-  --     variant = "moon",
-  --     --- @usage 'main'|'moon'|'dawn'
-  --     dark_variant = "main",
-  --     bold_vert_split = false,
-  --     dim_nc_background = false,
-  --     disable_background = false,
-  --     disable_float_background = false,
-  --     disable_italics = false,
-  --
-  --     --- @usage string hex value or named color from rosepinetheme.com/palette
-  --     groups = {
-  --       background = "base",
-  --       background_nc = "_experimental_nc",
-  --       panel = "surface",
-  --       panel_nc = "base",
-  --       border = "highlight_med",
-  --       comment = "muted",
-  --       link = "iris",
-  --       punctuation = "subtle",
-  --
-  --       error = "love",
-  --       hint = "iris",
-  --       info = "foam",
-  --       warn = "gold",
-  --
-  --       headings = {
-  --         h1 = "iris",
-  --         h2 = "foam",
-  --         h3 = "rose",
-  --         h4 = "gold",
-  --         h5 = "pine",
-  --         h6 = "foam",
-  --       },
-  --       -- or set all headings at once
-  --       -- headings = 'subtle'
-  --     },
-  --
-  --     -- Change specific vim highlight groups
-  --     -- https://github.com/rose-pine/neovim/wiki/Recipes
-  --     highlight_groups = {
-  --       ColorColumn = { bg = "rose", blend = 20 },
-  --
-  --       -- Blend colours against the "base" background
-  --       CursorLine = { bg = "foam", blend = 20 },
-  --       StatusLine = { fg = "love", bg = "love", blend = 10 },
-  --
-  --       -- By default each group adds to the existing config.
-  --       -- If you only want to set what is written in this config exactly,
-  --       -- you can set the inherit option:
-  --       Search = { bg = "gold", inherit = false },
-  --     },
-  --   },
-  -- },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      --- @usage 'auto'|'main'|'moon'|'dawn'
+      variant = "moon",
+      --- @usage 'main'|'moon'|'dawn'
+      dark_variant = "main",
+      bold_vert_split = false,
+      dim_nc_background = false,
+      disable_background = false,
+      disable_float_background = false,
+      disable_italics = false,
+
+      --- @usage string hex value or named color from rosepinetheme.com/palette
+      groups = {
+        background = "base",
+        background_nc = "_experimental_nc",
+        panel = "surface",
+        panel_nc = "base",
+        border = "highlight_med",
+        comment = "muted",
+        link = "iris",
+        punctuation = "subtle",
+
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        warn = "gold",
+
+        headings = {
+          h1 = "iris",
+          h2 = "foam",
+          h3 = "rose",
+          h4 = "gold",
+          h5 = "pine",
+          h6 = "foam",
+        },
+        -- or set all headings at once
+        -- headings = 'subtle'
+      },
+
+      -- Change specific vim highlight groups
+      -- https://github.com/rose-pine/neovim/wiki/Recipes
+      highlight_groups = {
+        ColorColumn = { bg = "rose", blend = 20 },
+
+        -- Blend colours against the "base" background
+        CursorLine = { bg = "foam", blend = 20 },
+        StatusLine = { fg = "love", bg = "love", blend = 10 },
+
+        -- By default each group adds to the existing config.
+        -- If you only want to set what is written in this config exactly,
+        -- you can set the inherit option:
+        Search = { bg = "gold", inherit = false },
+      },
+    },
+  },
   -- {
   --   "projekt0n/github-nvim-theme",
   -- },
@@ -176,9 +176,23 @@ return {
   --   },
   -- },
   {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "dracula",
     },
   },
 }
